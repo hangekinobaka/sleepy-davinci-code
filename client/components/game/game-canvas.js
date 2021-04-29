7d6bf9306cf74b03d779c5d7fca5c7a12da548f1
Irene <hangekinobaka@gmail.com>
2021-04-29 13:59:57 +0900

    [develop] handle window resize

--- client/components/game/game-canvas.js
+++ client/components/game/game-canvas.js
@@ -2,21 +2,48 @@ import { useEffect, useState } from 'react'
 import * as PIXI from 'pixi.js'
 import styles from 'styles/game.module.scss'
 
-export default function GameCanvas() {
-  useEffect(()=>{
+import {DESIGN_WIDTH,DESIGN_HEIGHT} from 'configs/variables'
+const ratio = DESIGN_HEIGHT / DESIGN_WIDTH
 
-    const app = new PIXI.Application({
-      width: 800, 
-      height: 600, 
-      backgroundColor: 0x1099bb, 
-      // autoResize: true,
-      // resizeTo: window
+export default function GameCanvas() {
+
+  const [w, setW] = useState(window.innerWidth)
+  const [h, setH] = useState(window.innerHeight)
+  const [app, setApp] = useState(new PIXI.Application({
+    width: w, 
+    height: w * ratio, 
+    backgroundColor: 0x1099bb, 
+    resizeTo: window,
+    autoResize: true
+  }))
+  const [card, setCard] = useState()
+
+  useEffect(()=>{
+    // Window init handler
+    window.addEventListener('resize', function(event){ 
+      setW(window.innerWidth)
+      setH(window.innerHeight)
     })
-    // app.renderer.resize(w, h)
+
     document.getElementById('game-canvas').appendChild(app.view)
-    
     const container = new PIXI.Container()
-  },[])
+    app.stage.addChild(container)
+
+    const texture = PIXI.Texture.from('/card-w.png')
+    const card = new PIXI.Sprite(texture)
+    setCard(card)
+   
+    container.addChild(card)
+  },[app])
+
+  useEffect(()=>{
+    // Window resize handler
+    if(!app || !card) return
+    console.log(card)
+    card.x = w / 2
+    card.y = h / 2 
+    card.anchor.set(0.5)
+  },[w, h])
 
   return(
     <div id="game-canvas" className={styles['game-canvas']}></div>
--- client/configs/variables.js
+++ client/configs/variables.js
@@ -9,3 +9,6 @@ export const API_CODE_ROOM_GEN_FAIL = 3
 export const API_CODE_NO_ROOM = 4
 export const API_CODE_ROOM_TAKEN = 5
 export const API_CODE_ROOM_DESTROYED = 6
+
+export const DESIGN_WIDTH = 2436
+export const DESIGN_HEIGHT = 1125

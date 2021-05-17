8155f05c20dfa6a1f31631095553c9be142c2829
Irene <hangekinobaka@gmail.com>
2021-05-18 02:27:56 +0900

    [develop] Update to version 1.0.0

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 # bin/bash
 
 docker-compose down
-docker pull hangekinobaka/dvc-app-client:0.0.1
-docker pull hangekinobaka/dvc-app-server:0.0.1
+docker pull hangekinobaka/dvc-app-client:1.0.0
+docker pull hangekinobaka/dvc-app-server:1.0.0
 docker-compose up -d --build
 docker image prune -f

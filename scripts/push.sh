9adbcdc9adbdeba1c4260a8f34f13c73bbe2f243
Irene <hangekinobaka@gmail.com>
2021-05-18 02:27:29 +0900

    [develop] Update to version 1.0.0

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,4 +1,4 @@
 # bin/bash
 
-docker push hangekinobaka/dvc-app-server:0.0.1
-docker push hangekinobaka/dvc-app-client:0.0.1
+docker push hangekinobaka/dvc-app-server:1.0.0
+docker push hangekinobaka/dvc-app-client:1.0.0
